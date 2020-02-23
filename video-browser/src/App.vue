5350e0ec5353abd8503a3bef4a976a4c51b6cdf7
hoodielive <hoodielive@gmail.com>
2020-02-23 12:43:44 -0500

    oh boy

---
+++ video-browser/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<SearchBar></SearchBar>
+	</div>
+</template>
+
+<script>
+import SearchBar from './components/SearchBar'
+
+export default {
+	name: 'App',
+	components: {
+		SearchBar
+	}
+};
+</script>
---
+++ video-browser/src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<input v-on:input="onInput" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SearchBar',
+	methods: {
+		onInput: function(event) {
+			console.log(event.target.value);
+		}
+	}
+};
+</script>
+
+<style>
+</style>
