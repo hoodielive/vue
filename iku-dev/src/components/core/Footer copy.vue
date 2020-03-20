905feff32daf8216e633de2382b17c1c210d49c0
hoodielive <hoodielive@gmail.com>
2020-03-20 19:22:19 -0400

    try a different footer

---
+++ iku-dev/src/components/core/Footer copy.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-footer
+    class="py-4"
+    dark
+    height="auto"
+  >
+    <v-container mx-auto>
+      <v-layout wrap>
+        <v-flex xs9>
+          <base-btn
+            v-for="(item, i) in items"
+            :key="i"
+            :href="item.href"
+            class="ml-0 mr-3"
+            color="primary"
+            square
+            target="_blank"
+          >
+            <v-icon v-text="item.icon" />
+          </base-btn>
+        </v-flex>
+        <v-spacer />
+        <base-btn
+          class="mr-0"
+          square
+          title="Go to top"
+          @click="$vuetify.goTo(0)"
+        >
+          <v-icon>mdi-chevron-up</v-icon>
+        </base-btn>
+      </v-layout>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        {
+          href: 'http://www.twitter.com',
+          icon: 'mdi-twitter'
+        },
+        {
+          href: 'http://www.instagram.com',
+          icon: 'mdi-instagram'
+        },
+        {
+          href: 'http://www.facebook.com',
+          icon: 'mdi-facebook'
+        },
+      ]
+    })
+  }
+</script>
--- iku-dev/src/components/core/Footer.vue
+++ iku-dev/src/components/core/Footer.vue
@@ -1,55 +1,33 @@
 <template>
   <v-footer
-    class="py-4"
     dark
-    height="auto"
+    padless
   >
-    <v-container mx-auto>
-      <v-layout wrap>
-        <v-flex xs9>
-          <base-btn
-            v-for="(item, i) in items"
-            :key="i"
-            :href="item.href"
-            class="ml-0 mr-3"
-            color="primary"
-            square
-            target="_blank"
-          >
-            <v-icon v-text="item.icon" />
-          </base-btn>
-        </v-flex>
-        <v-spacer />
-        <base-btn
-          class="mr-0"
-          square
-          title="Go to top"
-          @click="$vuetify.goTo(0)"
+    <v-card
+      flat
+      tile
+      class="hunter-green lighten-1 white--text text-center"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
         >
-          <v-icon>mdi-chevron-up</v-icon>
-        </base-btn>
-      </v-layout>
-    </v-container>
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>The Iku Organization</strong>
+      </v-card-text>
+    </v-card>
   </v-footer>
 </template>
-
-<script>
-  export default {
-    data: () => ({
-      items: [
-        {
-          href: 'http://www.twitter.com',
-          icon: 'mdi-twitter'
-        },
-        {
-          href: 'http://www.instagram.com',
-          icon: 'mdi-instagram'
-        },
-        {
-          href: 'http://www.facebook.com',
-          icon: 'mdi-facebook'
-        },
-      ]
-    })
-  }
-</script>
