daf2e6e25aa98aec27686c5df0a3205acd94ef2b
hoodielive <hoodielive@gmail.com>
2020-03-22 06:22:27 -0400

    delete - start over

--- vuetifyme/src/main.js
+++ vuetifyme/src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
