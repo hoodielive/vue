983214fa4ca702b70baa1436bc7e681b1995635f
hoodielive <hoodielive@gmail.com>
2020-03-22 13:20:11 -0400

    what in the world?

---
+++ vuetifyme/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+   <nav>
+    <v-toolbar flat dark app>
+        <v-toolbar-title class="text-uppercase grey--text">
+            <span class="font-weight-light">Iku</span>
+            <span>Organization</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn color="grey">
+            <span>Sign Out</span>
+            <v-icon right>mdi-login-variant</v-icon>
+        </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer v-model="drawer" app class="indigo">
+        <p>test</p>
+
+    </v-navigation-drawer>
+   </nav> 
+</template>
+
+<script>
+export default {
+    data() => {
+        drawer: false
+    }
+}
+</script>
