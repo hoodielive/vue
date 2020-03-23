71c01250cdea4189157fd592246372fab8d3b94f
hoodielive <hoodielive@gmail.com>
2020-03-22 21:03:37 -0400

    changes

--- vuetifyme/src/components/Navbar.vue
+++ vuetifyme/src/components/Navbar.vue
@@ -1,27 +1,59 @@
 <template>
    <nav>
-    <v-toolbar flat dark app>
+    <v-toolbar flat app>
+      <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
         <v-toolbar-title class="text-uppercase grey--text">
             <span class="font-weight-light">Iku</span>
             <span>Organization</span>
         </v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn color="grey">
+        <v-btn color="white">
             <span>Sign Out</span>
             <v-icon right>mdi-login-variant</v-icon>
         </v-btn>
     </v-toolbar>
-    <v-navigation-drawer v-model="drawer" app class="indigo">
-        <p>test</p>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+      <v-list-item-group
+        v-model="group"
+        active-class="deep-purple--text text--accent-4"
+      >
+        <v-list-item>
+          <v-list-item-title>Foo</v-list-item-title>
+        </v-list-item>
 
+        <v-list-item>
+          <v-list-item-title>Bar</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title>Fizz</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title>Buzz</v-list-item-title>
+        </v-list-item>
+
+      </v-list-item-group>
+      </v-list>
     </v-navigation-drawer>
-   </nav> 
+   </nav>
 </template>
 
 <script>
 export default {
-    data() => {
-        drawer: false
+    data () {
+      return {
+        drawer: true
+      }
     }
 }
 </script>
