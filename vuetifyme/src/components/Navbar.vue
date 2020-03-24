7455650e52f545100e26e62b5e951210637e035c
hoodielive <hoodielive@gmail.com>
2020-03-23 21:50:52 -0400

    removed the navigation-drawer, don't like it

--- vuetifyme/src/components/Navbar.vue
+++ vuetifyme/src/components/Navbar.vue
@@ -12,39 +12,6 @@
             <v-icon right>mdi-login-variant</v-icon>
         </v-btn>
     </v-toolbar>
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      bottom
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-      <v-list-item-group
-        v-model="group"
-        active-class="deep-purple--text text--accent-4"
-      >
-        <v-list-item>
-          <v-list-item-title>Foo</v-list-item-title>
-        </v-list-item>
-
-        <v-list-item>
-          <v-list-item-title>Bar</v-list-item-title>
-        </v-list-item>
-
-        <v-list-item>
-          <v-list-item-title>Fizz</v-list-item-title>
-        </v-list-item>
-
-        <v-list-item>
-          <v-list-item-title>Buzz</v-list-item-title>
-        </v-list-item>
-
-      </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
    </nav>
 </template>
 
--- vuetifyme/src/views/Home.vue
+++ vuetifyme/src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <h1>HomePage</h1>
 
   </div>
 </template>
