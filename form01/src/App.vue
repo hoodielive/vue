df8e2569ab3bfd7de651587fcc2814b6f0e998d3
hoodielive <hoodielive@gmail.com>
2020-03-25 22:06:58 -0400

    I used v-model to bind data from input to data object

---
+++ form01/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app" class="container py-4">
+    <div class="row">
+      <div class="col-12">
+        <form>
+          <div class="form-group">
+            <label>First Name:</label>
+            <input
+              v-model="form.firstName"
+              type="text"
+              class="form-control"
+            >
+          </div>
+
+          <div class="form-group">
+            <label>Last Name:</label>
+            <input
+              v-model="form.lastName"
+              type="text"
+              class="form-control"
+            >
+          </div>
+
+          <div class="form-group">
+            <label>Email:</label>
+            <input
+              v-model="form.email"
+              type="email"
+              class="form-control"
+            >
+          </div>
+
+          <div class="form-group">
+            <button class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      form: {
+        firstName: '',
+        lastName: '',
+        email: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
