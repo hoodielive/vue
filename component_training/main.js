52529313d469eb11e8c7dc94c33ee9e1b3dfe0e7
hoodielive <hoodielive@gmail.com>
2020-03-27 23:51:36 -0400

    the hell is going on

--- component_training/main.js
+++ component_training/main.js
@@ -1,7 +1,25 @@
-Vue.component('news', {
-  template: '<li>news.text</li>'
+Vue.component('task-list', {
+  template: `
+  <div>
+    <task v-for="task in tasks" :key="task"> {{ task.task }} </task>
+  </div>
+  `
+
+  data() {
+    return {
+      tasks: [
+        { task: "Go to the store", isComplete: false },
+        { task: "Go to Nigeria", isComplete: true },
+        { task: "Go to Columbia", isComplete: true }
+      ]
+    }
+  }
+})
+
+Vue.component('task', {
+  template: <li><slot></slot></li>
 })
 
 new Vue({
-  el: '#root',
+  el: '#root'
 })
