12c3c8775ba1c112daf2674956137a815431a703
hoodielive <hoodielive@gmail.com>
2020-03-30 11:21:01 -0400

    very exciting..

--- laravue/01-First/step2/main.js
+++ laravue/01-First/step2/main.js
@@ -1,27 +1,4 @@
-Vue.component('task-list', {
 
-  template: `
-      <div>
-        <task v-for="task in tasks"> {{ task.task }} </task>
-      </div>
-    `,
-  data() {
-    return {
-      tasks: [
-        { task: 'Go to the store.', complete: true },
-        { task: 'Go to the email.', complete: false },
-        { task: 'Go to the farm.', complete: true },
-        { task: 'Go to work.', complete: false }
-      ]
-    }
-  }
-
-})
-
-Vue.component('task', {
-
-  template: '<li><slot></slot></li>'
-})
 
 new Vue({
 
