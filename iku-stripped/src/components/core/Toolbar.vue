51df71efe7abb52a821b6df2b97dc38560959a57
hoodielive <hoodielive@gmail.com>
2020-04-05 21:21:45 -0400

    Whose the rabbit and who is the turtle

---
+++ iku-stripped/src/components/core/Toolbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-toolbar
+    app
+    flat
+  >
+    <v-toolbar-side-icon
+      class="hidden-md-and-up"
+      @click="toggleDrawer"
+    />
+    <v-container
+      mx-auto
+      py-0
+    >
+      <v-layout>
+        <v-btn
+          v-for="(link, i) in links"
+          :key="i"
+          :to="link.to"
+          class="ml-0 hidden-sm-and-down"
+          flat
+          @click="onClick($event, item)"
+        >
+          {{ link.text }}
+        </v-btn>
+        <v-spacer />
+        <v-text-field
+          append-icon="mdi-magnify"
+          flat
+          hide-details
+          solo-inverted
+          style="max-width: 300px;"
+        />
+      </v-layout>
+    </v-container>
+  </v-toolbar>
+</template>
+
+<script>
+  // Utilities
+  import {
+    mapGetters,
+    mapMutations
+  } from 'vuex'
+
+  export default {
+    computed: {
+      ...mapGetters(['links'])
+    },
+
+    methods: {
+      ...mapMutations(['toggleDrawer']),
+      onClick (e, item) {
+        e.stopPropagation()
+
+        if (item.to || !item.href) return
+
+        this.$vuetify.goTo(item.href)
+      }
+    }
+  }
+</script>
---
+++ iku-stripped/src/components/home/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container
+    id="about"
+    tag="section"
+  >
+    <base-subheading >
+      About Us
+    </base-subheading>
+    <p>
+      The Iku Organization are men restoring masculinity. This organization exists to restore rights of passage with an emphasis on teaching young men how to fish, hunt, handle firearms safely and more. We are a community oriented and can be seen helping the elderly, widows and orphans. If you are interested in joining our effort, please join now. If you would like to contribute, please donate. Thank you for stopping by.
+    </p>
+  </v-container>
+</template>
