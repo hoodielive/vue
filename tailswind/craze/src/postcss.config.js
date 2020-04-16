63fe1aac2bc85a7e8b99035ba1a390166b037d1a
hoodielive <hoodielive@gmail.com>
2020-04-16 19:25:48 -0400

    Added autoprefixer and other plugs for tailwindcss

--- tailswind/craze/src/postcss.config.js
+++ tailswind/craze/src/postcss.config.js
@@ -1,3 +1,14 @@
+const purgecss = require("@fullhuman/postcss-purgecss") ({
+	content: [
+		"./src/**/*.html",
+		"./src/**/*.vue",
+	],
+
+	defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
-	plugins: [require("tailwindcss"), require("autoprefixer")]
+	plugins: [require("tailwindcss"), require("autoprefixer"),
+	...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+]
 }
