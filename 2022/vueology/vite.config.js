4b69bd77e9a953e6c720902f6ccaf6348db21e2f
hoodielive <hoodielive@gmail.com>
2022-12-18 18:35:06 -0500

    I remember why I loved you Vue

---
+++ 2022/vueology/vite.config.js
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+});
+
+module.exports = {
+	devServer: {
+		port: 5173,
+		host: "10.0.1.8",
+	},
+};
---
+++ 2022/vueology/vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	devServer: {
+		port: 5173,
+		host: "10.0.1.8",
+	},
+};
