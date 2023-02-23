7b7651a29fcfc1786371871d6012673fe608efc2
hoodielive <hoodielive@gmail.com>
2023-02-22 21:31:47 -0500

    Okay

---
+++ 2023/standard/Main.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import { ref, onMounted } from 'vue'
+
+    const count = ref(0)
+
+    function increment() {
+        count.value++
+    }
+    onMounted(() => {
+        console.log(`The initial count is ${count.value}.`)
+    })
+</script>
+
+<template>
+    <button @click="increment">Count is: {{ count }}</button>
+</template>
